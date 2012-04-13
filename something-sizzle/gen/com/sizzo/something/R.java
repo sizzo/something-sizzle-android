3f59d24a688ef052581fdcad93c808a185b2cab6
Conan Chen <sizzo.cm@gmail.com>
2012-04-13 22:50:33 +0800

    create dynamic&static optionsMenus

--- something-sizzle/gen/com/sizzo/something/R.java
+++ something-sizzle/gen/com/sizzo/something/R.java
@@ -13,11 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int about=0x7f060000;
+        public static final int exit=0x7f060001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int main_menu=0x7f050000;
+    }
     public static final class string {
+        public static final int aboutOption=0x7f040003;
         public static final int app_name=0x7f040001;
+        public static final int dynamicUserHomeOption=0x7f040004;
+        public static final int exitOption=0x7f040002;
         public static final int hello=0x7f040000;
     }
 }
--- something-sizzle/src/main/java/com/sizzo/something/HelloAndroidActivity.java
+++ something-sizzle/src/main/java/com/sizzo/something/HelloAndroidActivity.java
@@ -3,23 +3,62 @@ package com.sizzo.something;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HelloAndroidActivity extends Activity {
 
-    private static String TAG = "something-sizzle";
+	private static final int ID_MENU_DYNAMIC_USER_HOME = 0;
+	private static String TAG = "something-sizzle";
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i(TAG, "onCreate");
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// creates a menu inflater
+		MenuInflater inflater = getMenuInflater();
+		// generates a Menu from a menu resource file
+		// R.menu.main_menu represents the ID of the XML resource file
+		inflater.inflate(R.menu.main_menu, menu);
+
+		appendDynamicOptionsMenu(menu);
+		return true;
+	}
+
+	private void appendDynamicOptionsMenu(Menu menu) {
+		// the menu option text is defined in resources
+		menu.add(Menu.NONE, ID_MENU_DYNAMIC_USER_HOME, Menu.NONE, R.string.dynamicUserHomeOption);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// check selected menu item
+		// R.id.exit is @+id/exit
+		if (item.getItemId() == R.id.exit) {
+			// close the Activity
+			this.finish();
+			return true;
+		} else if (item.getItemId() == ID_MENU_DYNAMIC_USER_HOME) {
+			Toast.makeText(this.getApplicationContext(),
+					"You just select RAINBOW menu, will redirect to rainbow home page", 30).show();
+		}
+		return false;
+	}
 
 }
-
