3fbce02f99b55f3664d9b86a0e3e3bd16e85b84c
Conan Chen <sizzo.cm@gmail.com>
2012-04-20 22:00:03 +0800

    custom wifi listview

--- something-sizzle/src/main/java/com/sizzo/something/wifi/WiFiScanReceiver.java
+++ something-sizzle/src/main/java/com/sizzo/something/wifi/WiFiScanReceiver.java
@@ -1,6 +1,9 @@
 package com.sizzo.something.wifi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -12,11 +15,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.SimpleAdapter;
 
 import com.sizzo.something.BrowseActivity;
+import com.sizzo.something.R;
 
 public class WiFiScanReceiver extends BroadcastReceiver {
 	private static final String TAG = "WiFiScanReceiver";
@@ -51,8 +54,22 @@ public class WiFiScanReceiver extends BroadcastReceiver {
 
 		String message = String.format("%s networks found. %s is the strongest.", wifiList.size(), bestSignal.SSID);
 		// Toast.makeText(c, message, Toast.LENGTH_LONG).show();
+		List<Map<String, Object>> contents = new ArrayList<Map<String, Object>>();
 
-		listView.setAdapter(new ArrayAdapter<ScanResult>(c, android.R.layout.test_list_item, wifiList));
+		for (ScanResult result : wifiList) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("PIC", R.drawable.pic);
+			map.put("TITLE", result.SSID);
+			map.put("CONTENT", result.toString());
+			contents.add(map);
+		}
+		SimpleAdapter adapter = new SimpleAdapter(parentActivity,
+				(List<Map<String, Object>>) contents, R.layout.listitem,
+				new String[] { "PIC", "TITLE", "CONTENT" }, new int[] {
+						R.id.listitem_pic, R.id.listitem_title,
+						R.id.listitem_content });
+
+		listView.setAdapter(adapter);
 		Log.d(TAG, "onReceive() message: " + message);
 	}
 
